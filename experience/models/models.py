74c4b30bf4256189d5ba7fd0293b967c78f506d4
nurlanf <nurlan.farajov@gmail.com>
2018-12-25 23:40:51 +0100

    UPDATE

--- experience/models/models.py
+++ experience/models/models.py
@@ -22,15 +22,15 @@ class Experience(models.Model):
             d2 = date.today()
             self.experience = relativedelta(d2, d1).years
 
+
 class Evaluation(models.Model):
     _inherit = 'hr.payslip'
 
-    organization_skill = fields.Selection([["1","1"],["2","2"],["3","3"],["4","4"],["5","5"]])
-    operational_excellence = fields.Selection([["1","1"],["2","2"],["3","3"],["4","4"],["5","5"]])
+    organization_skill = fields.Selection([["1", "1"], ["2", "2"], ["3", "3"], ["4", "4"], ["5", "5"]])
+    operational_excellence = fields.Selection([["1", "1"], ["2", "2"], ["3", "3"], ["4", "4"], ["5", "5"]])
     avarage_rate = fields.Float(string="Avarage rate", readonly=True, compute='_get_avarage')
 
     @api.multi
-    def _get_avarage( self ):
-
+    def _get_avarage(self):
         for record in self:
-            record['average_rate'] = ((record['organization_skill'] + record['operational_excellence']) / 2 )
+            record.average_rate = (record.organization_skill + record.operational_excellence) / 2
