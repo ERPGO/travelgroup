f6a0679cddb0be62ba80c988177a7d15dfb279de
nurlanf <nurlan.farajov@gmail.com>
2018-12-27 16:46:37 +0100

    Fix

--- payroll_timesheet/models/models.py
+++ payroll_timesheet/models/models.py
@@ -4,5 +4,17 @@ from odoo import models, api, fields
 class hr_employee(models.Model):
     _inherit = 'hr.payslip'
 
-    timesheet_ids = fields.One2many('account.analytic.line', 'employee_id', 'Timesheets')
-    employee_ids = fields.One2many('hr.employee', 'employee_id', 'Employees')
+    timesheet_ids = fields.One2many('account.analytic.line', compute="_get_timesheets")
+
+    @api.one
+    def _get_timesheets(self):
+        timesheets = self.env["account.analytic.line"].search([('employee_id', '=', self.employee_id)])
+        self.timesheet_ids = timesheets
+
+
+    employee_ids = fields.One2many('hr.employee', compute="_get_employees")
+
+    @api.one
+    def _get_employees(self):
+        employee_recordset = self.env["hr.employee"].search([('name', '=', self.employee_id.name)])
+        self.employee_ids = employee_recordset
