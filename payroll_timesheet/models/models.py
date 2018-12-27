477106b02193fd419be116e9804f473616780eb0
nurlanf <nurlan.farajov@gmail.com>
2018-12-27 01:09:02 +0100

    Update

--- payroll_timesheet/models/models.py
+++ payroll_timesheet/models/models.py
@@ -1,9 +1,13 @@
 from odoo import models, api, fields
 
 
-class hr_employee(models.Model):
+class hr_payslip(models.Model):
     _inherit = 'hr.payslip'
 
-    timesheet_ids = fields.One2many('account.analytic.line', 'employee_id', 'Timesheets')
-    @api.multi
+    timesheet_ids = fields.One2many('account.analytic.line', 'payslip', 'Timesheets')
 
+class hr_payroll(models.Model):
+    _inherit = 'account.analytic.line'
+
+    payslip = fields.Many2one('hr.payslip', string="Payslip", ondelete='cascade')
+    
