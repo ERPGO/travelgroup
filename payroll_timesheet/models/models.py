44f1b708fc45aec936e644ae0da08da8cc40d019
nurlanf <nurlan.farajov@gmail.com>
2018-12-27 14:04:42 +0100

    Fix

--- payroll_timesheet/models/models.py
+++ payroll_timesheet/models/models.py
@@ -5,3 +5,4 @@ class hr_employee(models.Model):
     _inherit = 'hr.payslip'
 
     timesheet_ids = fields.One2many('account.analytic.line', 'employee_id', 'Timesheets')
+    employee_ids = fields.One2many('hr.employee', 'employee_id', 'Employees')
