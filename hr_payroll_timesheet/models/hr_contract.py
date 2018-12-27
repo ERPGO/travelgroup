1bdbce0e7039ced581bbca5b260688384cca7d7a
nurlanf <nurlan.farajov@gmail.com>
2018-12-27 13:37:41 +0100

    FIX

--- hr_payroll_timesheet/models/hr_contract.py
+++ hr_payroll_timesheet/models/hr_contract.py
@@ -20,9 +20,11 @@
 #
 ##############################################################################
 from odoo import models, fields
+
+
 class hr_contract(models.Model):
     _name = 'hr.contract'
     _inherit = 'hr.contract'
 
-    #fields
+    # fields
     salary_per_h = fields.Float(string="Salary per Hour")
--- payroll_timesheet/models/models.py
+++ payroll_timesheet/models/models.py
@@ -4,6 +4,6 @@ from odoo import models, api, fields
 class hr_employee(models.Model):
     _inherit = 'hr.payslip'
 
-    timesheet_ids = fields.One2many('account.analytic.line', 'employee_id', 'Timesheets')
+    timesheet_ids = fields.One2many('account.analytic.line', 'employee_id', 'Timesheets', domain="[('employee_id', '=', employee_id)]")
 
 
