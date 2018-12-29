f44af2a12f0c7a382fc208b74c84c5718b227019
nurlanf <nurlan.farajov@gmail.com>
2018-12-29 16:46:24 +0100

    Update

--- payroll_timesheet/models/hr_payslip.py
+++ payroll_timesheet/models/hr_payslip.py
@@ -5,9 +5,7 @@ class hr_payslip(models.Model):
     _inherit = 'hr.payslip'
 
     timesheet_ids = fields.One2many('account.analytic.line', compute="_get_timesheets")
-    account_ids = fields.One2many('account.analytic.line', string="Sum of Timesheet hours",
-                           description="Sum of Timesheet hours",
-                           compute="_sum_timesheets")
+
 
     @api.one
     def _get_timesheets(self):
@@ -16,7 +14,16 @@ class hr_payslip(models.Model):
              ('validated', '=', True)])
         self.timesheet_ids = timesheets
 
+
+    account_ids = fields.One2many('account.analytic.line', string="Sum of Timesheet hours",
+                           description="Sum of Timesheet hours",
+                           compute="_sum_timesheets")
+
     @api.one
     def _sum_timesheets(self):
         all_account_recordset = self.env["account.analytic.line"].search([('account_id', '!=', None)])
         self.account_ids = all_account_recordset.mapped('account_id')
+
+
+    sum_timesheets = fields.One2many(''account.analytic.line', compute="_total_timesheets"')
+
