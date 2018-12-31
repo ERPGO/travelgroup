fd324d6006c24cec2decc6f801b7132b1b7c142a
nurlanf <nurlan.farajov@gmail.com>
2018-12-31 15:36:46 +0100

    Update

--- payroll_timesheet/models/hr_payslip.py
+++ payroll_timesheet/models/hr_payslip.py
@@ -4,14 +4,13 @@ from odoo import models, api, fields, _
 class hr_payslip(models.Model):
     _inherit = 'hr.payslip'
 
-    timesheet_ids = fields.One2many('account.analytic.line', compute="_get_timesheets",
-                                    domain="[('account_id','=','Vizam')]")
+    timesheet_ids = fields.One2many('account.analytic.line', compute="_get_timesheets")
 
     @api.one
     def _get_timesheets(self):
         timesheets = self.env["account.analytic.line"].search(
-            [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from), ('date', '<=', self.date_to),
-             ('validated', '=', True)])
+            '&', [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from), ('date', '<=', self.date_to),
+             ('validated', '=', True), '|', ('account_id', '=', 'Vizam'), ('account_id', '=', 'API')])
         self.timesheet_ids = timesheets
 
     api_timesheet_hours = fields.Float(string="API total hours", compute="_api_timesheets_sum")
