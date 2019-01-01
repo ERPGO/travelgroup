ebce8a61964cfd6d6402c4d0800493e88b355756
nurlanf <nurlan.farajov@gmail.com>
2019-01-01 23:23:59 +0100

    Update

--- payroll_timesheet/models/hr_payslip.py
+++ payroll_timesheet/models/hr_payslip.py
@@ -90,9 +90,8 @@ class hr_payslip(models.Model):
             payslip.line_ids.unlink()
             # set the list of contract for which the rules have to be applied
             # if we don't give the contract, then the rules to apply should be for all current contracts of the employee
-            contract_ids = not (
-                        not payslip.contract_id.ids and not self.get_contract(payslip.employee_id, payslip.date_from,
-                                                                              payslip.date_to))
+            contract_ids = payslip.contract_id.ids or \
+                self.get_contract(payslip.employee_id, payslip.date_from, payslip.date_to)
             lines = [(0, 0, line) for line in self._get_payslip_lines(contract_ids, payslip.id)]
             payslip.write({'line_ids': lines, 'number': number})
 
