46cb9c6ba3300e56ddafa706f79af645b9a081a4
nurlanf <nurlan.farajov@gmail.com>
2019-01-01 15:45:00 +0100

    Update

--- payroll_timesheet/models/hr_payslip.py
+++ payroll_timesheet/models/hr_payslip.py
@@ -7,7 +7,7 @@ class hr_payslip(models.Model):
     timesheet_ids = fields.One2many('account.analytic.line', compute="_get_timesheets")
 
     @api.one
-    def _get_timesheets(self):
+    def _get_timesheets( self ):
         timesheets = self.env["account.analytic.line"].search(
             [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from), ('date', '<=', self.date_to),
              ('validated', '=', True)])
@@ -16,15 +16,26 @@ class hr_payslip(models.Model):
     total_project_hours = fields.Float(string="Total project hours", compute="_total_timesheets_sum")
 
     @api.depends('timesheet_ids')
-    def _total_timesheets_sum(self):
+    def _total_timesheets_sum( self ):
         for obj in self:
             sum = 0.0
             for unit in self.timesheet_ids:
                 sum += unit.unit_amount
             obj.update({'total_project_hours': sum})
 
-    total_num_projects = fields.Integer(string="Total Project numbers", compute="_def_num_projects")
+    total_api_project = fields.Integer(string="Total Project numbers", compute="_def_num_projects")
 
     @api.multi
-    def _def_num_projects(self):
+    def _def_num_projects( self ):
         self.total_num_projects = len(self.timesheet_ids.mapped('project_id'))
+
+    api_project_hours = fields.Float(string="API project hours", compute="_api_timesheet_sum")
+
+    @api.depends('timesheet_ids')
+    def _api_timesheet_sum( self ):
+        api_timesheet_ids = self.timesheet_ids.search('account_id', '=', 'API')
+        for obj in self:
+            sum = 0.0
+            for unit in api_timesheet_ids:
+                sum += unit.unit_amount
+            obj.update({'api_project_hours': sum})
