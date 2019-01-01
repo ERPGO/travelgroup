6d2a19e5258a42b517627bc16916ede6adcc6b66
nurlanf <nurlan.farajov@gmail.com>
2019-01-01 22:49:46 +0100

    Update

--- payroll_timesheet/models/hr_payslip.py
+++ payroll_timesheet/models/hr_payslip.py
@@ -83,7 +83,7 @@ class hr_payslip(models.Model):
             self.backpack_percentage = backpack_split
 
     @api.multi
-    def compute_sheet(self):
+    def compute_sheet( self ):
         for payslip in self:
             number = payslip.number or self.env['ir.sequence'].next_by_code('salary.slip')
             # delete old payslip lines
@@ -91,7 +91,11 @@ class hr_payslip(models.Model):
             # set the list of contract for which the rules have to be applied
             # if we don't give the contract, then the rules to apply should be for all current contracts of the employee
             contract_ids = payslip.contract_id.ids or \
-                self.get_contract(payslip.employee_id, payslip.date_from, payslip.date_to)
+                           self.get_contract(payslip.employee_id, payslip.date_from, payslip.date_to)
             lines = [(0, 0, line) for line in self._get_payslip_lines(contract_ids, payslip.id)]
             payslip.write({'line_ids': lines, 'number': number})
+            for line in lines:
+                if line.code is API:
+                    line.rate = self.api_percentage
+
         return True
