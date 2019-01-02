e8c79cc2c51be0a6a966501c74995352ee315e21
nurlanf <nurlan.farajov@gmail.com>
2019-01-02 16:50:24 +0100

    Update

--- payroll_timesheet/models/hr_payslip.py
+++ payroll_timesheet/models/hr_payslip.py
@@ -7,7 +7,7 @@ class hr_payslip(models.Model):
     timesheet_ids = fields.One2many('account.analytic.line', compute="_get_timesheets")
 
     @api.one
-    def _get_timesheets( self ):
+    def _get_timesheets(self):
         timesheets = self.env["account.analytic.line"].search(
             [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from), ('date', '<=', self.date_to),
              ('validated', '=', True)])
@@ -16,7 +16,7 @@ class hr_payslip(models.Model):
     total_project_hours = fields.Float(string="Total project hours", compute="_total_timesheets_sum")
 
     @api.depends('timesheet_ids')
-    def _total_timesheets_sum( self ):
+    def _total_timesheets_sum(self):
         for obj in self:
             sum = 0.0
             for unit in self.timesheet_ids:
@@ -26,13 +26,13 @@ class hr_payslip(models.Model):
     total_num_projects = fields.Integer(string="Total Project numbers", compute="_def_num_projects")
 
     @api.multi
-    def _def_num_projects( self ):
+    def _def_num_projects(self):
         self.total_num_projects = len(self.timesheet_ids.mapped('project_id'))
 
     api_project_hours = fields.Float(string="API project hours", compute="_api_timesheet_sum")
 
     @api.depends('timesheet_ids')
-    def _api_timesheet_sum( self ):
+    def _api_timesheet_sum(self):
         api_timesheet_ids = self.env["account.analytic.line"].search(
             [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from), ('date', '<=', self.date_to),
              ('validated', '=', True), ('account_id', '=', 'API')])
@@ -45,7 +45,7 @@ class hr_payslip(models.Model):
     vizam_project_hours = fields.Float(string="Vizam project hours", compute="_vizam_timesheet_sum")
 
     @api.depends('timesheet_ids')
-    def _vizam_timesheet_sum( self ):
+    def _vizam_timesheet_sum(self):
         vizam_timesheet_ids = self.env["account.analytic.line"].search(
             [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from), ('date', '<=', self.date_to),
              ('validated', '=', True), ('account_id', '=', 'Vizam')])
@@ -58,7 +58,7 @@ class hr_payslip(models.Model):
     backpack_project_hours = fields.Float(string="BackPack project hours", compute="_backpack_timesheet_sum")
 
     @api.depends('timesheet_ids')
-    def _backpack_timesheet_sum( self ):
+    def _backpack_timesheet_sum(self):
         backpack_timesheet_ids = self.env["account.analytic.line"].search(
             [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from), ('date', '<=', self.date_to),
              ('validated', '=', True), ('account_id', '=', 'BackPack')])
@@ -73,7 +73,7 @@ class hr_payslip(models.Model):
     backpack_percentage = fields.Float(string="BackPack split", compute="_project_percentage")
 
     @api.depends('total_project_hours')
-    def _project_percentage( self ):
+    def _project_percentage(self):
         if self.total_project_hours > float(0):
             api_split = self.api_project_hours / self.total_project_hours * 100
             self.api_percentage = api_split
