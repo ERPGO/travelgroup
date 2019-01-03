08b5b5b07e6d2cd96b43f5465c2fca4056fd0d1b
nurlanf <nurlan.farajov@gmail.com>
2019-01-03 13:52:43 +0100

    New commit

---
+++ bonus/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "payroll_timesheet_bonus",
+
+    'summary': """
+        Bonus calculation addon to payroll_timesheet module
+    """,
+
+    'description': """
+        Bonus calculation addon to payroll_timesheet module
+    """,
+
+    'author': "ERGPO",
+    'website': "http://www.erpgo.az",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'hr', 'payroll_timesheet'],
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/crm2invoice.xml',
+        #'views/automation.xml',
+        #'views/avarage_rate.xml',
+        #'views/experience.xml',
+        'views/payroll_timesheet.xml',
+        # 'views/employees_list.xml',
+        # 'views/analytic_account.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+    'installable': True,
+    'Application': False,
+}
---
+++ bonus/models/models.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# noinspection PyUnresolvedReferences,PyUnresolvedReferences,PyUnresolvedReferences,PyUnresolvedReferences
+from odoo import models, fields, api, _
+from dateutil.relativedelta import relativedelta
+from datetime import date, datetime
+
+
+class Experience(models.Model):
+    _inherit = 'hr.employee'
+
+    startdate = fields.Date(string='Start Date')
+
+    experience = fields.Integer(string="Experience(years)")
+
+    @api.onchange('startdate')
+    def _set_experience( self ):
+        """Updates experience field when Start date is changed"""
+
+        if self.startdate:
+            d1 = datetime.strptime(str(self.startdate), "%Y-%m-%d").date()
+            d2 = date.today()
+            self.experience = relativedelta(d2, d1).years
+
+
+class Evaluation(models.Model):
+    _inherit = 'payroll_timesheet.bonus'
+
+    organization_skill = fields.Selection([["1", "1"], ["2", "2"], ["3", "3"], ["4", "4"], ["5", "5"]])
+    operational_excellence = fields.Selection([["1", "1"], ["2", "2"], ["3", "3"], ["4", "4"], ["5", "5"]])
+    kpi_score = fields.Float(string="KPI Score", readonly=True, stored=True, compute='_get_avarage')
+
+    employee_id = fields.Many2one('hr.employee', string="Employee")
+    payslip_id = fields.Many2one('hr.payslip', string="Payslip")
+    experience = fields.Float(related='hr.employee.experience', string="Experience(years)", readonly=True)
+
+
+
+    @api.multi
+    @api.onchange('organization_skill', 'operational_excellence')
+    def _get_avarage(self):
+        for record in self:
+            if record.organization_skill and record.operational_excellence:
+                record['avarage_rate'] = (int(record['organization_skill']) + int(record['operational_excellence'])) / 2
