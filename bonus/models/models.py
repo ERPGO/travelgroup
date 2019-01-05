9c7ace11fc8f38f6f3d3fd23923f6768d7316432
nurlanf <nurlan.farajov@gmail.com>
2019-01-05 21:29:31 +0100

    Update

--- bonus/models/models.py
+++ bonus/models/models.py
@@ -15,7 +15,7 @@ class Experience(models.Model):
     experience = fields.Integer(string="Experience(years)", stored=True)
 
     @api.onchange('startdate')
-    def _set_experience(self):
+    def _set_experience( self ):
         """Updates experience field when Start date is changed"""
 
         if self.startdate:
@@ -43,7 +43,7 @@ class Evaluation(models.Model):
     total_kpi = fields.Float(string="Total KPI", compute="_get_total_kpi")
 
     @api.depends('evaluation_lines')
-    def _get_total_kpi(self):
+    def _get_total_kpi( self ):
         for obj in self:
             sum = 0.0
             for unit in self.evaluation_lines:
@@ -53,7 +53,7 @@ class Evaluation(models.Model):
     total_experience = fields.Integer(string="Total Experience", compute="_get_total_experience")
 
     @api.depends('evaluation_lines')
-    def _get_total_experience(self):
+    def _get_total_experience( self ):
         for obj in self:
             sum = 0.0
             for unit in self.evaluation_lines:
@@ -65,7 +65,7 @@ class EvaluationLine(models.Model):
     _name = 'employee_evaluation.line'
     _description = 'Evaluation Lines'
 
-    evaluation_id = fields.Many2one('employee_evaluation')
+    evaluation_id = fields.Many2one('employee_evaluation', string="Evaluation")
     employee_id = fields.Many2one('hr.employee', domain="[('is_bonus_eligible', '=', True)]")
     organization_skill = fields.Selection([["1", "1"], ["2", "2"], ["3", "3"], ["4", "4"], ["5", "5"]])
     operational_excellence = fields.Selection([["1", "1"], ["2", "2"], ["3", "3"], ["4", "4"], ["5", "5"]])
@@ -73,7 +73,7 @@ class EvaluationLine(models.Model):
     experience = fields.Integer(related='employee_id.experience', string="Experience(years)", readonly=True)
 
     @api.onchange('organization_skill', 'operational_excellence')
-    def _get_avarage(self):
+    def _get_avarage( self ):
         for record in self:
             if record.organization_skill and record.operational_excellence:
                 record['kpi_score'] = (int(record['organization_skill']) + int(record['operational_excellence'])) / 2
@@ -83,10 +83,14 @@ class HRPayslipEval(models.Model):
     _inherit = 'hr.payslip'
 
     evaluation_id = fields.Many2one('employee_evaluation', string="Evaluation")
-    evaluation_lines = fields.One2many(string="Employee Evaluations", related='evaluation_id.evaluation_lines')
+    evaluation_lines = fields.One2many(string="Employee Evaluations", compute="_get_employees_evaluations")
     bonus_amount = fields.Float(string="Bonus Amount", related='evaluation_id.bonus_amount')
     total_kpi = fields.Float(string="Total KPI score", related='evaluation_id.total_kpi')
     total_experience = fields.Integer(related='evaluation_id.total_experience', string="Total Experience")
 
-
-
+    @api.multi
+    @api.depends('evaluation_id')
+    def _get_employees_evaluations( self ):
+        lines = self.env["employee_evaluation.line"].search(
+            [('employee_id', '=', self.employee_id.name), ('evaluaton_id', '=', self.evaluation_id.name)])
+        self.evaluation_lines = lines
