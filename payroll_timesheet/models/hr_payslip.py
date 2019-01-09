dc949719403fc65935411576d33bc4bc013b62ab
nurlanf <nurlan.farajov@gmail.com>
2019-01-09 17:00:49 +0100

    Update

--- payroll_timesheet/models/hr_payslip.py
+++ payroll_timesheet/models/hr_payslip.py
@@ -4,20 +4,21 @@ from odoo import models, api, fields, _
 class hr_payslip_projects(models.Model):
     _name = 'hr.payslip.projects'
     _description = 'to see total hours per project on hr.payslip module'
-    
+
     name = fields.Char(string="Description")
     project_id = fields.Many2one('project.project')
     project_hours = fields.Float(string="Total hours")
     overtime_hours = fields.Float(string="Overtime hours")
     slip_id = fields.Many2one('hr.payslip')
-    
+
+
 class hr_payslip(models.Model):
     _inherit = 'hr.payslip'
 
     timesheet_ids = fields.One2many('account.analytic.line', compute="_get_timesheets")
 
     @api.one
-    def _get_timesheets( self ):
+    def _get_timesheets(self):
         timesheets = self.env["account.analytic.line"].search(
             [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from), ('date', '<=', self.date_to),
              ('validated', '=', True)])
@@ -26,7 +27,7 @@ class hr_payslip(models.Model):
     total_project_hours = fields.Float(string="Total project hours", compute="_total_timesheets_sum")
 
     @api.depends('timesheet_ids')
-    def _total_timesheets_sum( self ):
+    def _total_timesheets_sum(self):
         for obj in self:
             sum = 0.0
             for unit in self.timesheet_ids:
@@ -36,23 +37,23 @@ class hr_payslip(models.Model):
     total_num_projects = fields.Integer(string="Total Project numbers", compute="_def_num_projects")
 
     @api.multi
-    def _def_num_projects( self ):
+    def _def_num_projects(self):
         self.total_num_projects = len(self.timesheet_ids.mapped('project_id'))
 
-
-        
     all_project_hours = fields.One2many('hr.payslip.projects', 'slip_id', "Project Hours")
-    
+
     @api.onchange('employee_id')
     def _calculate_project_hours(self):
         all_project_hours = []
         value = {}
         projects = self.env['project.project'].search([])
         for project in projects:
-            domain = [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from), ('date', '<=', self.date_to),
-            ('validated', '=', True), ('project_id', '=', project.name)]
-            domain_ot = [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from), ('date', '<=', self.date_to),
-            ('validated', '=', True), ('project_id', '=', project.name), ('task_id', '=', 'Overtime')]
+            domain = [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from),
+                      ('date', '<=', self.date_to),
+                      ('validated', '=', True), ('project_id', '=', project.name)]
+            domain_ot = [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from),
+                         ('date', '<=', self.date_to),
+                         ('validated', '=', True), ('project_id', '=', project.name), ('task_id', '=', 'Overtime')]
             all_timesheets = self.env["account.analytic.line"].search(domain)
             ot_timesheets = self.env["account.analytic.line"].search(domain_ot)
             sum_all = 0.0
@@ -61,15 +62,15 @@ class hr_payslip(models.Model):
                 sum_all += unit.unit_amount
             for ot in ot_timesheets:
                 sum_ot += ot.unit_amount
-            all_project_hours.append((0, 0, {'project_id': project.id, 'project_hours': sum_all, 'overtime_hours': sum_ot}))
+            all_project_hours.append(
+                (0, 0, {'project_id': project.id, 'project_hours': sum_all, 'overtime_hours': sum_ot}))
         value.update(all_project_hours=all_project_hours)
         return {'value': value}
-    
-    
+
     api_project_hours = fields.Float(string="API project hours", compute="_api_timesheet_sum")
 
     @api.depends('timesheet_ids')
-    def _api_timesheet_sum( self ):
+    def _api_timesheet_sum(self):
         api_timesheet_ids = self.env["account.analytic.line"].search(
             [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from), ('date', '<=', self.date_to),
              ('validated', '=', True), ('account_id', '=', 'API')])
@@ -82,7 +83,7 @@ class hr_payslip(models.Model):
     vizam_project_hours = fields.Float(string="Vizam project hours", compute="_vizam_timesheet_sum")
 
     @api.depends('timesheet_ids')
-    def _vizam_timesheet_sum( self ):
+    def _vizam_timesheet_sum(self):
         vizam_timesheet_ids = self.env["account.analytic.line"].search(
             [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from), ('date', '<=', self.date_to),
              ('validated', '=', True), ('account_id', '=', 'Vizam')])
@@ -95,7 +96,7 @@ class hr_payslip(models.Model):
     backpack_project_hours = fields.Float(string="BackPack project hours", compute="_backpack_timesheet_sum")
 
     @api.depends('timesheet_ids')
-    def _backpack_timesheet_sum( self ):
+    def _backpack_timesheet_sum(self):
         backpack_timesheet_ids = self.env["account.analytic.line"].search(
             [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from), ('date', '<=', self.date_to),
              ('validated', '=', True), ('account_id', '=', 'BackPack')])
@@ -110,7 +111,7 @@ class hr_payslip(models.Model):
     backpack_percentage = fields.Float(string="BackPack split", compute="_project_percentage")
 
     @api.depends('total_project_hours')
-    def _project_percentage( self ):
+    def _project_percentage(self):
         if self.total_project_hours > float(0):
             api_split = self.api_project_hours / self.total_project_hours * 100
             self.api_percentage = api_split
@@ -122,7 +123,7 @@ class hr_payslip(models.Model):
     ot_hours = fields.Float(string="OT hours", compute="_get_ot_hours")
 
     @api.multi
-    def _get_ot_hours( self ):
+    def _get_ot_hours(self):
         for obj in self:
             domain = [('employee_id', '=', self.employee_id.name), ('date', '>=', self.date_from),
                       ('date', '<=', self.date_to),
@@ -136,7 +137,7 @@ class hr_payslip(models.Model):
     total_ot_hours = fields.Float(string="Total OT hours", compute="_get_total_ot_hours")
 
     @api.multi
-    def _get_total_ot_hours( self ):
+    def _get_total_ot_hours(self):
         for obj in self:
             domain = [('date', '>=', self.date_from),
                       ('date', '<=', self.date_to),
@@ -146,4 +147,3 @@ class hr_payslip(models.Model):
             for unit in ot_timesheets_ids:
                 sum += unit.unit_amount
             obj.update({'total_ot_hours': sum})
-
